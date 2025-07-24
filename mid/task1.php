c25b5dd3c7fcacbf84c44af72951be3c11049eab
mashfika02 <mashfikarahman23@gmail.com>
2025-07-24 14:21:38 +0600

    added more lebel and input

--- mid/task1.php
+++ mid/task1.php
@@ -15,12 +15,30 @@
  <table>
     <form action="">
       <tr><td><p style="font-size: 20px;"><b><ins>Complete The Registration</ins></b></p></td></tr>
-
+<!--Full name-->
      <tr>
         <td> <label style="font-size:18px;" for="">Full Name :</label>
         </td>
         <td><input type="text" placeholder="Enter the Full Name"></td>
      </tr>
+<!--Email-->
+    <tr>
+        <td> <label style="font-size: 18px;" for="">Email :</label></td>
+        <td><input type="email" placeholder="Enter The Email"></td>
+    </tr>
+<!--Password-->
+<tr>
+    <td><label style="font-size: 18px;" for="password">Password :</label></td>
+    <td><input type="password" placeholder="Enter the Password" name="" id=""></td>
+</tr>
+<!--Gender-->
+    <td><label style="font-size:18px;" for="gender">Gender :</label></td>
+    <td>
+        <input type="radio" name="gender" id=""> Female
+        <input type="radio" name="gender" id=""> Male
+    </td>
+<!--lang-->
+      <td><label style="font-size: 18px;" for="language">Language Knowledge</label></td>
     </form>
  </table>
    
