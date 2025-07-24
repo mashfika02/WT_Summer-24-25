8abd0addd41e9bcd5c031b94b679791add920001
mashfika02 <mashfikarahman23@gmail.com>
2025-07-24 15:05:43 +0600

    task1 done

--- mid/task1.php
+++ mid/task1.php
@@ -38,10 +38,47 @@
         <input type="radio" name="gender" id=""> Male
     </td>
 <!--lang-->
+<tr>
       <td><label style="font-size: 18px;" for="language">Language Knowledge</label></td>
       <td>
-        
+        <input type="checkbox" name="lang" id=""> English
+        <input type="checkbox" name="lang" id=""> Bangla
+        <input type="checkbox" name="lang" id=""> hindi
       </td>
+</tr>
+<!--country-->
+<tr>
+    <td><label style="font-size: 18px;" for="country">Country :</label></td>
+    <td>
+        <select name=""id="">
+            <option value="">select</option>
+            <option value="">Bangladesh</option>
+            <option value="">US</option>
+        </select>
+    </td>
+</tr>
+<!--DOB-->
+<tr>
+    <td><label style="font-size: 18px;" for="DOB">Date of Birth :</label></td>
+    <td><input type="date" name="" id=""></td>
+</tr>
+<!--Dupload file-->
+<tr>
+    <td><label style="font-size: 18px;" for="upload photo">Upload Photo :</label></td>
+    <td><input type="file" name="" id=""></td>
+</tr>
+<!--comments-->
+<tr>
+    <td><label style="font-size: 18px;" for="comments">Comments :</label></td>
+    <td><textarea name="" id="" rows="5"></textarea></td>
+
+</tr>
+<!--submit button-->
+<tr>
+   <td>
+     <button  style="background-color: skyblue;"  type="submit">Submit</button>
+   </td>
+</tr>
     </form>
  </table>
    
