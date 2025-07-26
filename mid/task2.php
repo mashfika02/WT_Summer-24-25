e6d8fe70ffcc9bd9394bc7c999a88e8558bb84ba
mashfika02 <mashfikarahman23@gmail.com>
2025-07-26 10:49:22 +0600

    added style , class, id

--- mid/task2.php
+++ mid/task2.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>task 2</title>
+    <style>
+   .title{
+      color:#5D9C98;
+   }
+   #title2{
+    color: #5D9C98;
+    font-family: Arial, Helvetica, sans-serif;
+   }
+   
+   label,p{
+    font-size: 18px;
+    font-family: Arial, Helvetica, sans-serif;
+   }
+   input{
+    background-color: #B5D4D4;
+    border-radius: 5px;
+    padding: 5px;
+   }
+    </style>
 </head>
 <body>
     <center>
-        <h1>Bank Management System</h1>
+        <h1 class="title">Bank Management System</h1>
+        <h3 id="title2"><ins>Your Trusted Financial Partner</ins></h3>
     </center>
+
+    <table>
+        <form action="">
+         <tr><td><p>Customer Registration From</p></td></tr>
+<!--Full name-->
+         <tr>
+            <td><label for="name">Full Name :</label></td>
+            <td><input type="name" placeholder="Enter your Full Name"></td>
+         </tr>
+
+        </form>
+    </table>
 </body>
 </html>
