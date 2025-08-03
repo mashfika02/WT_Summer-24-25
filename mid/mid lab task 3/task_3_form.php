70c275bff05de23b83829d2dbd122c11a617fb83
mashfika02 <mashfikarahman23@gmail.com>
2025-08-03 10:52:23 +0600

    Added js file

--- mid/mid lab task 3/task_3_form.php
+++ mid/mid lab task 3/task_3_form.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mid lab Task 3</title>
     <link rel="stylesheet" href="task_3_style.css">
+    <script src="task_3_valid.js"></script>
 </head>
 <body>
    <div>
